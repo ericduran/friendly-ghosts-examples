30c04443fcbfcbc7ac0cb1a963b9d28242324438
Eric J. Duran <eric.duran7@gmail.com>
2012-06-21 20:58:55 -0300

    Update master

--- ajax_popup.js
+++ ajax_popup.js
@@ -2,21 +2,20 @@ casper.test.comment('Testing that we have an ajax popup');
 
 casper.start('http://www.marthastewart.com/', function() {
   
-  // this.test.assertDoesntExists('iframe.my-ms-frame', 'The iframe is not on the page');
   this.test.assertEval(function() {
       return document.querySelectorAll('.my-ms-frame').length == 0;
-  }, 'The is no iframe with the my-ms-frame class');
+  }, 'There is no iframe with the my-ms-frame class');
 
   // Now lets click the sign-in link.
   this.click('a.login-frame');
 
-  // Lets test that our remote content is not on the page.
+  // Lets test that our remote content is now on the page.
   this.test.assertEval(function() {
       return document.querySelectorAll('.my-ms-frame').length == 1;
-  }, 'There is now an iframe with the my-ms-frame class');
+  }, 'There is now an iframe with the my-ms-frame class in a pop-up');
 
 });
 
 casper.run(function() {
-    this.test.done(); // I must be called once all the async stuff has been executed
+    this.test.done();
 });
